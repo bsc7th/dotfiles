6eb1b2cb4f0a0f750e949262cc2b70edca82aeaf
bsc7th <ryanjayleyva@proton.me>
2025-03-20 06:59:43 -0400

    removed custom mappings

--- nvim/lua/bsc7th/plugins/surround.lua
+++ nvim/lua/bsc7th/plugins/surround.lua
@@ -3,15 +3,4 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   version = "*",
   config = true,
-  opts = {
-    mappings = {
-      add = "<leader>sa",
-      delete = "<leader>sd",
-      find = "gsf",
-      find_left = "gsF",
-      highlight = "gsh",
-      replace = "gsr",
-      update_n_lines = "gsn",
-    },
-  },
 }
