c89e5d2cc9092786b55b5b826dcd157db9fc14ca
bsc7th <ryanjayleyva@proton.me>
2025-03-20 04:28:19 -0400

    A plugin to search for and insert emojis/kaomojis, with auto-completion support, right from Neovim 😀

---
+++ nvim/lua/bsc7th/plugins/emoji.lua
@@ -0,0 +1,13 @@
+return {
+	{
+		"allaman/emoji.nvim",
+		version = "1.0.0",
+		ft = "markdown",
+		dependencies = {
+			"hrsh7th/nvim-cmp",
+		},
+		opts = {
+			enable_cmp_integration = true,
+		},
+	},
+}
