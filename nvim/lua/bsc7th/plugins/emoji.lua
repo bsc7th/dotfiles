420231b1c6f8dfc499328bb5c9ccf0ac1f209407
bsc7th <ryanjayleyva@proton.me>
2025-03-22 11:01:15 -0400

    add plenary and fzf

--- nvim/lua/bsc7th/plugins/emoji.lua
+++ nvim/lua/bsc7th/plugins/emoji.lua
@@ -1,13 +1,15 @@
 return {
-	{
-		"allaman/emoji.nvim",
-		version = "1.0.0",
-		ft = "markdown",
-		dependencies = {
-			"hrsh7th/nvim-cmp",
-		},
-		opts = {
-			enable_cmp_integration = true,
-		},
-	},
+  {
+    "allaman/emoji.nvim",
+    version = "1.0.0",
+    ft = "markdown",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+      "ibhagwan/fzf-lua",
+    },
+    opts = {
+      enable_cmp_integration = true,
+    },
+  },
 }
