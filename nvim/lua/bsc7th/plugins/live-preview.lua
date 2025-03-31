f131774c6139823e24b793a26406f337ae0d9bf6
bsc7th <ryanjayleyva@proton.me>
2025-03-31 11:56:49 -0400

    set port 3000 for live preview

--- nvim/lua/bsc7th/plugins/live-preview.lua
+++ nvim/lua/bsc7th/plugins/live-preview.lua
@@ -8,5 +8,10 @@ return {
     -- Set your key mappings for live preview
     map("n", "<leader>lp", "<cmd>LivePreview start<cr>", { noremap = true, silent = true })
     map("n", "<leader>lP", "<cmd>LivePreview close<cr>", { noremap = true, silent = true })
+
+    -- Set port for live preview
+    require("livepreview.config").set({
+      port = 3000, -- specify the port number here
+    })
   end,
 }
