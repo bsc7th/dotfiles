c4c817600d3aa635d3166ee85f632d4d2e270349
bsc7th <ryanjayleyva@proton.me>
2025-04-22 02:21:06 -0400

    optimize Vim settings

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,61 @@
+vim.g.mapleader = " "
+
+vim.cmd("let g:netrw_liststyle = 3")
+
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+vim.opt.shortmess:append("I")
+
+local opt = vim.opt
+
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+vim.g.snacks_animate = false
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.cursorline = false
+
+vim.opt.laststatus = 0
+
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+vim.opt.signcolumn = "yes"
+
+vim.opt.backspace = "indent,eol,start"
+
+opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus"
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.backup = false
+vim.opt.showcmd = true
+vim.opt.cmdheight = 1
+vim.opt.scrolloff = 10
+vim.opt.shell = "zsh"
+vim.opt.hlsearch = true
+vim.opt.splitkeep = "cursor"
+vim.opt.mouse = ""
+
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
+vim.cmd([[au BufNewFile,BufRead *.astro setf astro]])
+
+opt.swapfile = false
+
+if vim.fn.has("nvim-0.8") == 1 then
+  vim.opt.cmdheight = 0
+end
