d62f96d49b22c67591a6db8a881842715bc0686b
bsc7th <ryanjayleyva@proton.me>
2025-04-22 02:23:36 -0400

    enable `vim.loader` and set up custom print/debug function

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,10 @@
-require("bsc7th.core")
-require("bsc7th.lazy")
+if vim.loader then
+  vim.loader.enable()
+end
+
+_G.dd = function(...)
+  require("util.debug").dump(...)
+end
+vim.print = _G.dd
+
+require("config.lazy")
