42edac68e02a48cd1a6591da8253b9e5b24efea4
bsc7th <ryanjayleyva@proton.me>
2025-04-23 01:55:06 -0400

    leaving Osaka, welcome Tokyo!

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,15 +1,11 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
-
--- This is where you actually apply your config choices
 
 config.max_fps = 120
 
-config.color_scheme = "Solarized Dark - Patched"
--- config.color_scheme = "Tokyo Night Moon"
+-- config.color_scheme = "Solarized Dark - Patched"
+config.color_scheme = "Tokyo Night Moon"
 
 config.font = wezterm.font("PlemolJP Console NF")
 config.font_size = 18
@@ -21,5 +17,4 @@ config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.75
 config.macos_window_background_blur = 10
 
--- and finally, return the configuration to wezterm
 return config
