9b98fd0e4d47530282278f97419cbf414fb47cf3
bsc7th <ryanjayleyva@proton.me>
2025-04-26 04:58:58 -0400

    replaced `<leader>` with `;` for a smoother trouble experience

--- nvim/lua/bsc7th/plugins/trouble.lua
+++ nvim/lua/bsc7th/plugins/trouble.lua
@@ -6,14 +6,14 @@ return {
   },
   cmd = "Trouble",
   keys = {
-    { "<leader>xx", "<cmd>Trouble diagnostics toggle<CR>", desc = "Open trouble workspace diagnostics" },
+    { ";xx", "<cmd>Trouble diagnostics toggle<CR>", desc = "Open trouble workspace diagnostics" },
     {
-      "<leader>xd",
+      ";xd",
       "<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
       desc = "Open trouble document diagnostics",
     },
-    { "<leader>xf", "<cmd>Trouble quickfix toggle<CR>", desc = "Open trouble quickfix list" },
-    { "<leader>xl", "<cmd>Trouble loclist toggle<CR>", desc = "Open trouble location list" },
-    { "<leader>xt", "<cmd>Trouble todo toggle<CR>", desc = "Open todos in trouble" },
+    { ";xf", "<cmd>Trouble quickfix toggle<CR>", desc = "Open trouble quickfix list" },
+    { ";xl", "<cmd>Trouble loclist toggle<CR>", desc = "Open trouble location list" },
+    { ";xt", "<cmd>Trouble todo toggle<CR>", desc = "Open todos in trouble" },
   },
 }
