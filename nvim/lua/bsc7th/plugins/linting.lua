4c342d4a90093da0803fce39ed9e018be74e7cdc
bsc7th <ryanjayleyva@proton.me>
2025-04-27 11:29:07 -0400

    linting is ready to rock for JavaScript, TypeScript, and Astro too!

--- nvim/lua/bsc7th/plugins/linting.lua
+++ nvim/lua/bsc7th/plugins/linting.lua
@@ -1,14 +1,18 @@
 return {
   "mfussenegger/nvim-lint",
-  event = { "BufReadPre", "BufNewFile" },
+  event = {
+    "BufReadPre",
+    "BufNewFile",
+  },
   config = function()
     local lint = require("lint")
 
     lint.linters_by_ft = {
       javascript = { "eslint_d" },
       typescript = { "eslint_d" },
-      javascriptreact = { "eslint_d" },
-      typescriptreact = { "eslint_d" },
+      ["javascriptreact"] = { "eslint_d" },
+      ["typescriptreact"] = { "eslint_d" },
+      astro = { "eslint_d" },
       svelte = { "eslint_d" },
       python = { "pylint" },
     }
@@ -18,12 +22,20 @@ return {
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
       group = lint_augroup,
       callback = function()
-        lint.try_lint()
+        local ft = vim.bo.filetype
+        if lint.linters_by_ft[ft] ~= nil then
+          lint.try_lint()
+        end
       end,
     })
 
     vim.keymap.set("n", "<leader>ll", function()
-      lint.try_lint()
+      local ft = vim.bo.filetype
+      if lint.linters_by_ft[ft] ~= nil then
+        lint.try_lint()
+      else
+        vim.notify("No linter configured for this filetype: " .. ft, vim.log.levels.WARN)
+      end
     end, { desc = "Trigger linting for current file" })
   end,
 }
