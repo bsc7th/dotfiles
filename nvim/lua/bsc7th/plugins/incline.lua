d3ce342c051af7198ceb437992ffa6a4bb90ca03
bsc7th <ryanjayleyva@proton.me>
2025-04-28 14:02:58 -0400

    just vibes: cleaned up incline.nvim buffer build

--- nvim/lua/bsc7th/plugins/incline.lua
+++ nvim/lua/bsc7th/plugins/incline.lua
@@ -15,7 +15,9 @@ return {
         local ft_icon, ft_color = require("nvim-web-devicons").get_icon_color(filename)
         local modified = vim.bo[props.buf].modified
         local buffer = {
-          ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
+          ft_icon
+              and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) }
+            or "",
           " ",
           { filename, gui = modified and "bold,italic" or "bold" },
           " ",
