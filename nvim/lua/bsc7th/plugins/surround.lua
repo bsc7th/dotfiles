6a5a741454bfcbaab5718f7335d0a2d193b0ca50
bsc7th <ryanjayleyva@proton.me>
2025-04-29 07:38:42 -0400

    rope 'em and wrap 'em with custom keymaps

--- nvim/lua/bsc7th/plugins/surround.lua
+++ nvim/lua/bsc7th/plugins/surround.lua
@@ -7,6 +7,16 @@ return {
   config = function()
     require("nvim-surround").setup({
       -- Configuration here, or leave empty to use defaults
+      keymaps = {
+        normal = "ys",
+        normal_cur = "yss",
+        normal_line = "yS",
+        normal_cur_line = "ySS",
+        visual = "S",
+        visual_line = "gS",
+        delete = "ds",
+        change = "cs",
+      },
     })
   end,
 }
