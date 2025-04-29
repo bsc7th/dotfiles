8e653fc4edd20d0bc2780c7916daf882772f9ac3
bsc7th <ryanjayleyva@proton.me>
2025-04-29 19:14:38 -0400

    git it, Lazy!

--- nvim/lua/bsc7th/plugins/git/lazygit.lua
+++ nvim/lua/bsc7th/plugins/git/lazygit.lua
@@ -1,16 +1,20 @@
 return {
-  "kdheepak/lazygit.nvim",
+  "kdheepak/lazygit.nvim", -- Plugin that integrates LazyGit (a terminal UI for Git) into Neovim
   cmd = {
-    "LazyGit",
-    "LazyGitConfig",
-    "LazyGitCurrentFile",
-    "LazyGitFilter",
-    "LazyGitFilterCurrentFile",
+    "LazyGit", -- Opens LazyGit UI
+    "LazyGitConfig", -- Opens LazyGit with Git config view
+    "LazyGitCurrentFile", -- LazyGit focused on the current file
+    "LazyGitFilter", -- Filter commits in LazyGit
+    "LazyGitFilterCurrentFile", -- Filter commits of the current file
   },
   dependencies = {
-    "nvim-lua/plenary.nvim",
+    "nvim-lua/plenary.nvim", -- Required dependency that provides Lua utility functions
   },
   keys = {
-    { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
+    {
+      "<leader>lg",
+      "<cmd>LazyGit<cr>",
+      desc = "Open lazy git", -- Keybinding to open LazyGit with <leader>lg
+    },
   },
 }
