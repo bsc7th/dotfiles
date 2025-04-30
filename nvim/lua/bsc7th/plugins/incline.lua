6e2e8b5565adebe0d8d2f925b4629a291c7eb558
bsc7th <ryanjayleyva@proton.me>
2025-04-30 07:39:00 -0400

    lean, mean, and clean, no more icons.

--- nvim/lua/bsc7th/plugins/incline.lua
+++ nvim/lua/bsc7th/plugins/incline.lua
@@ -2,9 +2,7 @@ return {
   "b0o/incline.nvim",
   event = "BufReadPre",
   priority = 1200,
-  dependencies = { "nvim-tree/nvim-web-devicons", opts = {} },
   config = function()
-    local helpers = require("incline.helpers")
     require("incline").setup({
       window = {
         padding = 0,
@@ -12,12 +10,8 @@ return {
       },
       render = function(props)
         local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-        local ft_icon, ft_color = require("nvim-web-devicons").get_icon_color(filename)
         local modified = vim.bo[props.buf].modified
         local buffer = {
-          ft_icon
-              and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) }
-            or "",
           " ",
           { filename, gui = modified and "bold,italic" or "bold" },
           " ",
