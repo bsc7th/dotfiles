c3e57403de82ba77a47316f6db21857fd9763d41
bsc7th <ryanjayleyva@proton.me>
2025-04-30 06:12:10 -0400

    font with a touch of italic flair

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,19 +2,22 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.max_fps = 120
+config.window_decorations = "RESIZE"
+config.enable_tab_bar = false
+config.window_background_opacity = 0.75
+config.macos_window_background_blur = 10
 
--- config.color_scheme = "Solarized Dark - Patched"
-config.color_scheme = "Tokyo Night Moon"
+config.color_scheme = "tokyonight_night"
+config.max_fps = 120
 
 config.font = wezterm.font("PlemolJP Console NF")
 config.font_size = 18
 
-config.enable_tab_bar = false
-
-config.window_decorations = "RESIZE"
-
-config.window_background_opacity = 0.75
-config.macos_window_background_blur = 10
+config.font_rules = {
+	{
+		italic = true,
+		font = wezterm.font("PlemolJP Console NF", { italic = true }),
+	},
+}
 
 return config
