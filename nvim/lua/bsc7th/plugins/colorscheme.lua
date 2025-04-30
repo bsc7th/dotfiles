68cf135cffdcaf4b167ccd420180a2c9a0c71db5
bsc7th <ryanjayleyva@proton.me>
2025-04-30 05:22:31 -0400

    been to Tokyo and back, I need a nap!

--- nvim/lua/bsc7th/plugins/colorscheme.lua
+++ nvim/lua/bsc7th/plugins/colorscheme.lua
@@ -41,40 +41,38 @@ return {
     require("tokyonight").setup({
       style = "night",
       transparent = true,
-      border = "rounded",
       lualine_bold = true,
       use_background = "dark",
       terminal_colors = true,
       dim_inactive = true,
       cache = true,
-      enable_italics = true,
-
       light_style = "day",
       day_brightness = 0.1,
       plugins = {},
+      styles = {
+        comments = { italic = true },
+        keywords = { bold = true },
+        functions = { bold = true },
+        variables = { italic = true },
+        strings = { bold = true },
+        numbers = { bold = true },
+        operators = { bold = true },
+        types = { italic = true },
+        conditionals = { italic = true },
+        loops = { italic = true },
+        booleans = { bold = true },
+      },
+
       on_colors = function(colors)
         colors.border = "#565f89"
       end,
+
       on_highlights = function(highlights, colors)
         highlights.Comment = { fg = colors.comment, italic = true }
         highlights.Function = { fg = colors.blue, bold = true }
       end,
-
-      styles = {
-        boolean = { bold = true },
-        loops = { italic = true },
-        operators = { bold = true },
-        numbers = { bold = true },
-        functions = { bold = true },
-        keywords = { bold = true },
-        strings = { bold = true },
-        comments = { italic = true },
-        types = { italic = true },
-        variables = { italic = true },
-        underline = true,
-        undercurl = true,
-      },
     })
+
     vim.cmd("colorscheme tokyonight")
   end,
 }
