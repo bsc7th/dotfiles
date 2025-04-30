8b754f283d789de38dd3edeb390f95f1416b417f
bsc7th <ryanjayleyva@proton.me>
2025-04-30 02:03:55 -0400

    wrangling those surrounds with nvim-surround!

--- nvim/lua/bsc7th/plugins/surround.lua
+++ nvim/lua/bsc7th/plugins/surround.lua
@@ -6,7 +6,6 @@ return {
   -- version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
   config = function()
     require("nvim-surround").setup({
-      -- Configuration here, or leave empty to use defaults
       keymaps = {
         normal = "ys",
         normal_cur = "yss",
