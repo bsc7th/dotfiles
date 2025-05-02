c216e20435551516440b8b3b0a2d7abba083ef8d
bsc7th <ryanjayleyva@proton.me>
2025-05-02 07:30:48 -0400

    lazy-load render-markdown only for .md files with ft trigger

--- nvim/lua/bsc7th/plugins/markdown.lua
+++ nvim/lua/bsc7th/plugins/markdown.lua
@@ -2,6 +2,7 @@ return {
   {
     "MeanderingProgrammer/markdown.nvim",
     name = "render-markdown",
+    ft = { "markdown" },
     config = function()
       require("render-markdown").setup({
         html = { enabled = false },
