c61d955b9c0a59d310fa0523e4ff1de91f493663
bsc7th <ryanjayleyva@proton.me>
2025-05-05 17:53:43 -0400

    feat(options): add terminal color settings and Astro filetype detection

--- nvim/lua/bsc7th/core/options.lua
+++ nvim/lua/bsc7th/core/options.lua
@@ -1,6 +1,8 @@
 vim.g.mapleader = " "
-vim.opt.relativenumber = true
+vim.opt.mouse = ""
+vim.opt.splitkeep = "cursor"
 vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
@@ -14,19 +16,28 @@ vim.opt.laststatus = 0
 vim.opt.termguicolors = true
 vim.opt.background = "dark"
 vim.opt.signcolumn = "yes"
-vim.opt.backspace = "indent,eol,start"
-vim.opt.clipboard:append("unnamedplus")
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.backup = false
 vim.opt.showcmd = true
 vim.opt.cmdheight = 1
 vim.opt.scrolloff = 10
-vim.opt.shell = "zsh"
 vim.opt.hlsearch = true
-vim.opt.splitkeep = "cursor"
-vim.opt.mouse = ""
 vim.opt.swapfile = false
+vim.opt.shell = "zsh"
+vim.opt.backspace = "indent,eol,start"
+vim.opt.clipboard:append("unnamedplus")
 
 vim.opt.shortmess:append("I")
 vim.opt.formatoptions:append({ "r" })
+
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
+vim.cmd("set t_ZH=[3m")
+vim.cmd("set t_ZR=[23m")
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.astro",
+  command = "setfiletype astro",
+})
