63b45cbc9375e00f39338def7b0e6df9d5ca8df3
bsc7th <ryanjayleyva@proton.me>
2025-05-06 05:41:40 -0400

    chore(lualine): clean up unused statusline filetypes

--- nvim/lua/bsc7th/plugins/lualine.lua
+++ nvim/lua/bsc7th/plugins/lualine.lua
@@ -38,7 +38,7 @@ return {
         theme = "auto",
         globalstatus = vim.o.laststatus == 3,
         disabled_filetypes = {
-          statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" },
+          statusline = {},
         },
       },
       sections = {
