51051ae162d87ee7972dc9d027a76c3eccdc0dde
bsc7th <ryanjayleyva@proton.me>
2025-05-09 04:31:31 -0400

    refactor(mason): convert mason plugin setup to opts approach

--- nvim/lua/bsc7th/plugins/mason.lua
+++ nvim/lua/bsc7th/plugins/mason.lua
@@ -1,54 +1,54 @@
 return {
   "williamboman/mason.nvim",
-  -- NOTE: Lazy-loaded: tools won't auto-install. Run :Mason and install manually, or remove (event = "VeryLazy") line to auto-install on startup.
-  -- event = "VeryLazy",
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
-  config = function()
+  opts = function()
+    return {
+      mason = {
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+            check_outdated_packages_on_open = false,
+          },
+          border = "none",
+        },
+        log_level = vim.log.levels.WARN,
+      },
+      mason_lspconfig = {
+        ensure_installed = {
+          "html",
+          "cssls",
+          "lua_ls",
+          "ts_ls",
+          "astro",
+          "svelte",
+          "emmet_ls",
+          "graphql",
+          "prismals",
+        },
+        automatic_enable = false,
+      },
+      mason_tool_installer = {
+        ensure_installed = {
+          "prettier",
+          "eslint_d",
+          "stylua",
+        },
+        automatic_enable = true,
+      },
+    }
+  end,
+  config = function(_, opts)
     local mason = require("mason")
     local mason_lspconfig = require("mason-lspconfig")
     local mason_tool_installer = require("mason-tool-installer")
 
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-          check_outdated_packages_on_open = false,
-        },
-        border = "none",
-      },
-      log_level = vim.log.levels.WARN,
-    })
-
-    mason_lspconfig.setup({
-      ensure_installed = {
-        "html",
-        "cssls",
-        "lua_ls",
-        "ts_ls",
-        "astro",
-        "svelte",
-        "emmet_ls",
-        "graphql",
-        "prismals",
-        "htmx",
-        "gopls",
-      },
-      automatic_installation = true,
-    })
-
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "prettier",
-        "eslint_d",
-        "stylua",
-        "shfmt",
-      },
-      automatic_installation = true,
-    })
+    mason.setup(opts.mason)
+    mason_lspconfig.setup(opts.mason_lspconfig)
+    mason_tool_installer.setup(opts.mason_tool_installer)
   end,
 }
