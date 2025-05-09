a0eba61921b08c546031313fe3dea5f0cb94c424
bsc7th <ryanjayleyva@proton.me>
2025-05-09 04:33:38 -0400

    feat(lazy.lua): enable plugin checker in lazy.nvim setup

--- nvim/lua/bsc7th/lazy.lua
+++ nvim/lua/bsc7th/lazy.lua
@@ -11,4 +11,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("bsc7th.plugins")
+require("lazy").setup({
+  spec = {
+    { import = "bsc7th.plugins" },
+  },
+  checker = { enabled = true },
+})
