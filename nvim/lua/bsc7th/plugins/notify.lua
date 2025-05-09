ea9fc2469caa1c9f03db67eaf5c2faa07efdf3ff
bsc7th <ryanjayleyva@proton.me>
2025-05-09 16:08:06 -0400

    refactor(notify): use opts approach and simplify highlight group setup for lazy.nvim

--- nvim/lua/bsc7th/plugins/notify.lua
+++ nvim/lua/bsc7th/plugins/notify.lua
@@ -2,9 +2,29 @@ return {
   "rcarriga/nvim-notify",
   opts = {
     timeout = 5000,
-    stages = "fade_in_slide_out",
+    stages = "fade",
+    on_open = function()
+      local border_color = "#98971a"
+      local levels = {
+        ERROR = "#fb4934",
+        WARN = "#fabd2f",
+        INFO = "#83a598",
+        DEBUG = "#928374",
+        TRACE = "#d3869b",
+      }
+
+      for level, color in pairs(levels) do
+        vim.api.nvim_set_hl(0, "Notify" .. level .. "Icon", { fg = color })
+        vim.api.nvim_set_hl(0, "Notify" .. level .. "Title", { fg = color })
+        vim.api.nvim_set_hl(0, "Notify" .. level .. "Border", { fg = border_color })
+        vim.api.nvim_set_hl(0, "Notify" .. level .. "Body", { link = "Normal" })
+      end
+    end,
   },
-  init = function()
-    vim.notify = require("notify")
+  config = function(_, opts)
+    local notify = require("notify")
+    notify.setup(opts)
+    vim.notify = notify
+    vim.cmd("doautocmd ColorScheme")
   end,
 }
