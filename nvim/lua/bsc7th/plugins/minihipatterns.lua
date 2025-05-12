a460d90c85aaff1dad5b637370b002f3261f023b
bsc7th <ryanjayleyva@proton.me>
2025-05-12 04:30:52 -0400

    feat(nvim): add mini-hipatterns for highlighting hex color codes

---
+++ nvim/lua/bsc7th/plugins/minihipatterns.lua
@@ -0,0 +1,25 @@
+return {
+  "echasnovski/mini.hipatterns",
+  version = "*",
+  config = function()
+    local hipatterns = require("mini.hipatterns")
+
+    hipatterns.setup({
+      highlighters = {
+        -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+        fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+        hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+        todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+        note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+
+        -- Highlight hex color strings (`#rrggbb`) using that color
+        hex_color = hipatterns.gen_highlighter.hex_color(),
+      },
+      -- Delay configurations
+      delay = {
+        text_change = 200, -- Delay for text changes
+        scroll = 50, -- Delay for scroll updates
+      },
+    })
+  end,
+}
