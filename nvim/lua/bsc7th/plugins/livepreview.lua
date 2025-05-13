a6d6ad4f54484ecd7d0fabf1d86f33851b9c173c
bsc7th <ryanjayleyva@proton.me>
2025-05-13 03:17:01 -0400

    fix(livepreview.lua): fix typo lazy = "VeryLazy" to event = "VeryLazy"

--- nvim/lua/bsc7th/plugins/livepreview.lua
+++ nvim/lua/bsc7th/plugins/livepreview.lua
@@ -1,7 +1,7 @@
 return {
   "brianhuster/live-preview.nvim",
   version = "*",
-  lazy = "VeryLazy",
+  event = "VeryLazy",
   cmd = "LivePreview start",
   dependencies = {
     "ibhagwan/fzf-lua",
